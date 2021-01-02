97b884160188fd69a6c98c88c4e001a0aa173812
YuLiangGoh <40126252+YuLiangGoh@users.noreply.github.com>
2021-01-03 00:19:20 +0800

    homepage update

--- app/src/main/java/com/example/smarthome/MainActivity.java
+++ app/src/main/java/com/example/smarthome/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.smarthome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Find ID
+        name = findViewById(R.id.main_username);
+
+        //set Username
+        name.setText("Thomas Oliver");
     }
 }
