bc44777944a592ad74a86e14cdb5889acbb2aa98
Tzemin <40569118+tmin97@users.noreply.github.com>
2021-01-02 22:39:59 +0800

    delete test

--- app/src/main/java/com/example/smarthome/MainActivity.java
+++ app/src/main/java/com/example/smarthome/MainActivity.java
@@ -10,25 +10,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
-    private Button button;
-    private EditText editText;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.hello_text);
-        editText = findViewById(R.id.edit_text);
-        button = findViewById(R.id.button);
-
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText(editText.getText().toString().trim());
-
-            }
-        });
     }
 }
