b39030d64f1fb7a28067717d521aeb8735fc5b9e
lhw97 <hanweng123@gmail.com>
2021-01-04 01:11:40 +0800

    Update activity_living_room.xml with functioning seekbar

--- app/src/main/java/com/example/smarthome/LivingRoom.java
+++ app/src/main/java/com/example/smarthome/LivingRoom.java
@@ -8,13 +8,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
 
 public class LivingRoom extends Fragment {
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_living_room, container, false);
+
+        TextView currentTemperature = (TextView)rootView.findViewById(R.id.temperature);
+        SeekBar temperatureBar = (SeekBar)rootView.findViewById(R.id.temperatureBar);
+
+        temperatureBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                currentTemperature.setText(progress + "\u2103");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
         return  rootView;
+
+
     }
 }
