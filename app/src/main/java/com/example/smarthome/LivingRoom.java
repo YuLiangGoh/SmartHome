9ba163527d7c44118984cdfc6ce8e2d30ebbee13
YuLiangGoh <40126252+YuLiangGoh@users.noreply.github.com>
2021-01-04 14:29:50 +0800

    new

--- app/src/main/java/com/example/smarthome/LivingRoom.java
+++ app/src/main/java/com/example/smarthome/LivingRoom.java
@@ -18,32 +18,7 @@ public class LivingRoom extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_living_room, container, false);
-
-        TextView currentTemperature = (TextView)rootView.findViewById(R.id.temperature);
-        SeekBar temperatureBar = (SeekBar)rootView.findViewById(R.id.temperatureBar);
-
-        temperatureBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                currentTemperature.setText(progress + "\u2103");
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-
         return  rootView;
-
-
     }
 }
