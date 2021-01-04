1c361de95ed92e0e497fef4d1cc70af41c3a25a5
YuLiangGoh <40126252+YuLiangGoh@users.noreply.github.com>
2021-01-04 15:38:01 +0800

    aircon setting page

---
+++ app/src/main/java/com/example/smarthome/AirConditionerSetting.java
@@ -0,0 +1,14 @@
+package com.example.smarthome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AirConditionerSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_air_conditioner_setting);
+    }
+}
