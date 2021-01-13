81a19d80c38ff2f838d73381f39140f06c27d51b
lhw97 <hanweng123@gmail.com>
2021-01-14 00:06:53 +0800

    update laundry update kitchen

--- app/src/main/java/com/example/smarthome/LaundryRoom.java
+++ app/src/main/java/com/example/smarthome/LaundryRoom.java
@@ -5,17 +5,65 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.suke.widget.SwitchButton;
 
 public class LaundryRoom extends Fragment {
+
+    private SwitchButton laundrySwitch;
+    private TextView timer;
+    private SharedPreferences sharedPreferences;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_laundry_room, container, false);
+
+        laundrySwitch = rootView.findViewById(R.id.laundry_switch);
+        timer = rootView.findViewById(R.id.timer);
+
+        sharedPreferences = getActivity().getSharedPreferences("Laundry_Room", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        laundrySwitch.setOnCheckedChangeListener(new SwitchButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(SwitchButton view, boolean isChecked) {
+
+                if(isChecked){
+                    new CountDownTimer(3000, 1000){
+
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+                            if(!laundrySwitch.isChecked()){
+                                return;
+                            }
+                            double time_left = millisUntilFinished/1000+1;
+                            timer.setText("remaining time: " + time_left);
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            timer.setText("laundry is ready!");
+                        }
+                    }.start();
+                }
+
+                else{
+                    timer.setText("");
+                }
+            }
+        });
+
+
+
         return  rootView;
     }
 }
